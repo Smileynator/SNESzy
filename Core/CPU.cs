fc0a1b003759b39b614fd1a36e50ed1d8263a8d0
Smileynator <hoivince@hotmail.com>
2026-01-22 00:07:45 +0100

    3 instructions to end all of emulation mode.

--- Core/CPU.cs
+++ Core/CPU.cs
@@ -9,7 +9,7 @@ public class CPU
     private ushort PC, SP, directPage;
     private byte PCBank, dataBank;
     private bool emulation = true;
-    private bool carry, zero, irq, decmode, brk, overflow, negative;
+    private bool carry, zero, interrupt, decmode, indexSize, accSize, overflow, negative;
 
     private uint PCAddress => (uint)PCBank << 16 | PC;
 
@@ -21,12 +21,42 @@ public class CPU
 
     public void Initialize()
     {
+        interrupt = true;
+        indexSize = true;
+        accSize = true;
+        SP = 0x01FF;
         //read native mode reset vector
-        PC = (ushort)(busRead.Read(0x00FFFC) << 8 | busRead.Read(0x00FFFD));
+        PC = 0xFFFC;
+        PC = ReadImmediate16();
     }
 
     public void Tick()
     {
+        int cycles = 0;
         byte opcode = busRead.Read(PCAddress);
+        PC++;
+
+        switch (opcode)
+        {
+            case 0x18: //carry bit false
+                carry = false;
+                cycles += 2;
+                break;
+            case 0x78: //interrupts off
+                interrupt = false;
+                cycles += 2;
+                break;
+            case 0xFB: //exchange emulation and carry bits
+                (emulation, carry) = (carry, emulation);
+                cycles += 2;
+                break;
+        }
+    }
+
+    private ushort ReadImmediate16()
+    {
+        ushort result = (ushort) (busRead.Read(PCAddress) | (busRead.Read(PCAddress+1) << 8));
+        PC += 2;
+        return result;
     }
 }
